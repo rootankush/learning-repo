ecc10b39e43402b0b575c8ec0c1d4d3b160e41d6
rootankush <rootankushprompt@gmail.com>
2025-07-21 03:34:08 +0530

    Done Project

---
+++ color-changer/README.md
@@ -0,0 +1,22 @@
+# Color Scheme Switcher 
+
+A simple color scheme switcher built with HTML, CSS, and JavaScript.
+
+Click the color buttons to change the background color
+
+## Features
+- Color Change 
+- Clean UI
+
+## 🚀 Live Preview (Local)
+
+To run this project locally, you need [`live-server`](https://www.npmjs.com/package/live-server), a lightweight development server with live reload.
+
+### 🔧 Requirements
+
+- Node.js (for installing `live-server`)
+- Or install `live-server` globally using npm:
+
+```bash
+npm install -g live-server
+
---
+++ color-changer/src/main.js
@@ -0,0 +1,24 @@
+const greyBtn = document.getElementById("greyBtn");
+const whiteBtn = document.getElementById("whiteBtn");
+const blueBtn = document.getElementById("blueBtn");
+const yellowBtn = document.getElementById("yellowBtn");
+const para = document.getElementById("para");
+
+function greyColor(){
+  document.body.style.backgroundColor = "grey";
+};
+
+function whiteColor(){
+  document.body.style.backgroundColor = "white";
+};
+
+function blueColor(){
+  document.body.style.backgroundColor = "blue";
+};
+
+function yellowColor(){
+  document.body.style.backgroundColor = "yellow";
+};
+
+
+
